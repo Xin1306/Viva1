0292a80202990c9d256defecad669cd435ddf1bb
Xin1306 <149458454+Xin1306@users.noreply.github.com>
2023-11-07 13:46:15 +0800

    Question4

---
+++ Viva1.java
@@ -0,0 +1,38 @@
+
+package viva1;
+import java.util.Scanner;
+
+public class Viva1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String strength;
+        int upper=0,lower=0,special=0,digit=0;
+        System.out.println("Enter your password: ");
+        String password=sc.next();
+                
+        for(int i=0;i<password.length();i++){
+        char x1=password.charAt(i);
+   
+           if(Character.isLowerCase(x1))
+               lower++;
+           if(Character.isUpperCase(x1))
+               upper++;
+           if(x1=='!'||x1=='@'||x1=='#'||x1=='$'||x1=='%'||x1=='^'||x1=='&'||x1=='*'||x1=='('||x1==')'||x1=='-'||x1=='+')
+               special++;
+           if(Character.isDigit(x1))
+               digit++;
+           
+           }
+        if(upper>0&&lower>0&&special>0&&password.length()>=8&&digit>0)
+            strength="Strong";
+        
+        else if(upper>0&&lower>0&&special>0&&password.length()>=6)
+            strength="Moderate";
+        else
+            strength="Weak";
+        
+        System.out.println("Strength of password: "+strength);
+    }
+            
+}
+
